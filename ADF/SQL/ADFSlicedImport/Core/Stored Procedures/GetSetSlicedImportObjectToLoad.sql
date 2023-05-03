2cd75ae90fa238d521a1c37f00de539fccdd48e2
meinradweiss <meinrad.weiss@hotmail.com>
2023-05-03 09:34:08 +0200

    Filter support for @SlicedImportObject_Id added

--- ADF/SQL/ADFSlicedImport/Core/Stored Procedures/GetSetSlicedImportObjectToLoad.sql
+++ ADF/SQL/ADFSlicedImport/Core/Stored Procedures/GetSetSlicedImportObjectToLoad.sql
@@ -5,6 +5,7 @@ CREATE PROCEDURE [Core].[GetSetSlicedImportObjectToLoad]
     @SourceSystemName sysname
    ,@SourceSchema     sysname        ='%'
    ,@SourceObject     sysname        ='%'
+   ,@SlicedImportObject_Id varchar(64) = '%'
    ,@Mode             VARCHAR(25) = 'REGULAR'
 )
 AS
@@ -20,7 +21,8 @@ BEGIN
      OR (@Mode = 'RESTART'  AND	[LastStart] IS NOT NULL AND [LastSuccessEnd] IS NULL)
      OR (@Mode = 'ALL'))
     AND [SourceSchema] LIKE @SourceSchema
-    AND   [SourceObject] LIKE @SourceObject 
+    AND [SourceObject] LIKE @SourceObject 
+    AND CONVERT(VARCHAR(64), [SlicedImportObject_Id]) = @SlicedImportObject_Id 
 
 
 END
